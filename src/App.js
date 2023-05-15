57580b74a46fbabb29b644b4d121189b57016549
Castrozan <castro.lucas290@gmail.com>
2023-05-14 23:18:50 -0300

    Implementação teste google maps api

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { GoogleMap, MarkerF, useLoadScript } from "@react-google-maps/api";
+import { useMemo } from "react";
+import "./App.css";
+import vector from './5265402.png';
 
-function App() {
+const coordenadaSATC = { lat: -28.703918, lng: -49.406100 };
+const zoom = 18
+
+const App = () => {
+  const { isLoaded } = useLoadScript({
+    googleMapsApiKey: process.env.REACT_APP_GOOGLE_API_KEY,
+  });
+  const center = useMemo(() => (coordenadaSATC), []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      {!isLoaded ? (
+        <h1>Loading...</h1>
+      ) : (
+        <GoogleMap
+          mapContainerClassName="map-container"
+          center={center}
+          zoom={zoom}
         >
-          Learn React
-        </a>
-      </header>
+          <MarkerF position={coordenadaSATC} icon={vector} />
+        </GoogleMap>
+      )}
     </div>
   );
-}
+};
 
 export default App;
